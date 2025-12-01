7b6ded27b914472f224cec7cd04cc07785ab68af
Vladimir Bondarev <vladimir.bondarev@dinitcs.com>
2025-12-01 17:31:19 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static bool ValidateArgs(string[] args)
+    {
+        var argsAreValid = true;
+        if (args.Length != 3)
+        {
+            argsAreValid  = false;
+            Console.Error.WriteLine("3 args are expected");
+        }
+
+        if (args.Length < 1 || string.IsNullOrEmpty(args[0]))
+        {
+            argsAreValid  = false;
+            Console.Error.WriteLine("SolutionPath should be provided");
+        }
+        
+        if (args.Length < 2 || string.IsNullOrEmpty(args[1]))
+        {
+            argsAreValid  = false;
+            Console.Error.WriteLine("DependencyName should be provided");
+        }
+
+        if (args.Length < 3 || string.IsNullOrEmpty(args[2]))
+        {
+            argsAreValid  = false;
+            Console.Error.WriteLine("DependencyVersion should be provided");
+        }
+
+        if (!argsAreValid)
+        {
+            Console.WriteLine("Usage: dotnet observe [solution-path] [dependency-name] [version]");
+        }
+        
+        return argsAreValid;
+    }
+    
+    public static void Main(string[] args)
+    {
+        if (!ValidateArgs(args)) return;
+        
+        
+    }
+}
